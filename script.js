0acf938d6d14b86ed93cbb523d25d124425bf889
Soohwan Lee <shlee8405@gmail.com>
2022-09-05 16:02:42 +0900

    Added new game logic

--- script.js
+++ script.js
@@ -1,16 +1,131 @@
-console.log("hello world");
 
+
+let userWonRound = 0;
+let computerWonRound = 0;
+
+// Get computer's choice
 function getComputerChoice() {
-    var myChoice = ['Rock', 'Paper', 'Scissors'];
-    var randomChoice = myChoice[Math.floor(Math.random() * myChoice.length)];
+    var computerChoices = ['rock', 'paper', 'scissors'];
+    var randomChoice = computerChoices[Math.floor(Math.random() * computerChoices.length)];
     return randomChoice;
 }
 
-function getUserChoice() {
-    var userChoice = prompt();
-    return userChoice;
+//Get user's choice. If clicked wrong, return null.
+function userChoice(e) {
+    const choice = e.path[0].classList.value;
+    if (choice == 'rock' || choice == 'paper' || choice == 'scissors'){
+        return choice;
+    }
+    return;
 }
 
+//Starts game. Ommits no choice.
+function gameStart(e) {
+    var user = userChoice(e);
+    var computer = getComputerChoice();
+    var header = document.getElementsByClassName("gameResult")[0];
+    if (!user){
+        console.log("no choice");
+        header.textContent = "Don't avoid your Destiny!";
+    }
+    else {
+        gameLogic(user, computer);
+    }    
+}
+
+//Base game logit.
+function gameLogic(user, computer) {
+    var header = document.getElementsByClassName("gameResult")[0];
+    if (computer == 'rock') {
+        if (user == 'rock') {
+            console.log("It's a tie");
+            header.textContent = "It's a Tie!";
+        }
+        if (user =='paper'){
+            console.log('You win! Paper beats rock!');
+            userWin();
+        }
+        if (user =='scissors'){
+            console.log('You lose. Rock beats scissors');
+            computerWin();
+        }
+    }
+    if (computer == 'paper') {
+        if (user == 'rock') {
+            console.log("You lose. Paper beats rock.");
+            computerWin();
+        }
+        if (user =='paper'){
+            console.log("It's a tie!");
+            header.textContent = "It's a Tie!";
+        }
+        if (user =='scissors'){
+            console.log('You win! Scissors beat paper!');
+            userWin();
+        }
+    }
+    if (computer == 'scissors') {
+        if (user == 'rock'){
+            console.log("You win! Rock beats scissors");
+            userWin();
+        }
+        if (user =='paper'){
+            console.log('You lose. Scissors beat paper.');
+            computerWin();
+        }
+        if (user =='scissors'){
+            console.log("It's a tie.");
+            header.textContent = "It's a Tie!";
+        }
+    }
+}
+
+
+//User wins round. Adds to counter.
+function userWin() {
+    var userScore = document.getElementsByClassName("userScore")[0];
+    var header = document.getElementsByClassName("gameResult")[0];
+    userWonRound += 1;
+    userScore.textContent = "User Score : " + userWonRound;
+
+
+    header.textContent = "You Won!";
+
+    if (userWonRound == 5) {
+        alert("You WON!")
+    }
+
+}
+//Computer wins round. Adds to counter.
+function computerWin() {
+    var computerScore = document.getElementsByClassName("computerScore")[0];
+    var header = document.getElementsByClassName("gameResult")[0];
+    computerWonRound += 1;
+    computerScore.textContent ="Computer Score :" + computerWonRound;
+
+    header.textContent = "Computer Won!";
+
+
+    if (computerWonRound == 5) {
+        alert("You LOST!")
+    }
+}
+
+
+
+window.addEventListener('click', gameStart);
+
+
+/*
+const btns = document.querySelectorAll('button');
+
+function userChoice(e) {
+    console.log(this.classList.value);
+}
+
+btns.forEach(button => button.addEventListener('click', userChoice));  */
+
+/*
 function gameFunction() {
     var computerChoice = getComputerChoice();
     var userChoice = getUserChoice();
@@ -55,4 +170,5 @@ function gameFunction() {
 
 }
 
-console.log(gameFunction());
+*/
+//console.log(gameFunction());
